87f7d4d2c52d13313639ff4b5d5a48d8aa1edc5d
Jakub Wengrzyn <kubawen10@gmail.com>
2023-12-14 14:00:31 +0100

    Add project files.

---
+++ PWRekruter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PWRekruter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
