1070642b5b7a1e1a7a760f9a8cc78c8f7c7cf61a
Kasia <kasiawo02@gmail.com>
2024-01-17 20:49:29 +0100

    merge

---
+++ PWRekruter/Utils/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace PWRekruter.Utils
+{
+    public static class EnumHelper
+    {
+        public static string GetDisplayName(Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var displayAttribute = (DisplayAttribute)Attribute.GetCustomAttribute(field, typeof(DisplayAttribute));
+
+            return displayAttribute?.Name ?? value.ToString();
+        }
+    }
+}
